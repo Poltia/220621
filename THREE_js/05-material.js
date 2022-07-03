155607e7ae8ad2c292c6f9339f810c29d145d2f2
WooJonghwa <wjh423@gmail.com>
2022-07-03 18:23:22 +0900

    Mesh_Texture_Map

--- THREE_js/05-material.js
+++ THREE_js/05-material.js
@@ -2,6 +2,7 @@
 
 import * as THREE from '../THREE_js/three.module.js'; // three.module.js 파일을 모듈 버전으로 불러옴
 import { OrbitControls } from '../THREE_js/OrbitControls.js'; // 마우스로 화면 컨트롤 가능
+import { VertexNormalsHelper } from '../THREE_js/VertexNormalsHelper.js'
 
 class App {
     constructor() {
@@ -40,59 +41,75 @@ class App {
             0.1,
             100
         );
-        camera.position.z = 7;
+        camera.position.z = 3;
         this._camera = camera;
+        this._scene.add(camera);
     }
 
     _setupLight() {
+        const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+        this._scene.add(ambientLight);
+
         const color = 0xffffff;  //광원의 색상
         const intensity = 1;  //광원의 세기
         const light = new THREE.DirectionalLight(color, intensity);  //색상과 세기로 광원 생성
         light.position.set(-1, 2, 4); //광원의 위치
-        this._scene.add(light);
+        //this._scene.add(light);
+        this._camera.add(light);
     }
 
     _setupModel() {
         const textureLoader = new THREE.TextureLoader();
-        const map = textureLoader.load(
-            "../THREE_js/uv_grid_opengl.jpg",
-            texture => {
-                texture.repeat.x = 1;
-                texture.repeat.y = 1;
-
-                texture.wrapS = THREE.ClampToEdgeWrapping;
-                texture.wrapT = THREE.ClampToEdgeWrapping;
-
-                texture.offset.x = 0; //uv좌표의 시작위치 설정
-                texture.offset.y = 0;
-
-                texture.rotation = THREE.MathUtils.degToRad(0); //uv좌표 (0,0)을 기준으로 ()도만큼 반시계방향으로 돌림
-                texture.center.x = 0.5; //회전의 기준이 되는 uv좌표 설정. 기본은 0
-                texture.center.y = 0.5;
-
-                texture.magFilter = THREE.NearestFilter;
-                // minFilter의 종류. Mipmap이 선명도는 좋지만 연산이 많아 속도면에서 불리하기때문에 상황에 맞게 써야한다.
-                //texture.minFilter = THREE.NearestFilter;
-                //texture.minFilter = THREE.LinearFilter;
-                //texture.minFilter = THREE.NearestMipmapLinearFilter;
-                //texture.minFilter = THREE.NearestMipmapNearestFilter;
-                //texture.minFilter = THREE.LinearMipmapNearestFilter;
-                texture.minFilter = THREE.LinearMipmapLinearFilter;
-
-            }
-        );
+        const mapAO = textureLoader.load("../THREE_js/imgs/glass/Glass_Window_002_ambientOcclusion.jpg");
+        const map = textureLoader.load("../THREE_js/imgs/glass/Glass_Window_002_basecolor.jpg");
+        const mapHeight = textureLoader.load("../THREE_js/imgs/glass/Glass_Window_002_height.png");
+        const mapMetalic = textureLoader.load("../THREE_js/imgs/glass/Glass_Window_002_metallic.jpg");
+        const mapNormal = textureLoader.load("../THREE_js/imgs/glass/Glass_Window_002_normal.jpg");
+        const mapAlpha = textureLoader.load("../THREE_js/imgs/glass/Glass_Window_002_opacity.jpg");
+        const mapRoughness = textureLoader.load("../THREE_js/imgs/glass/Glass_Window_002_roughness.jpg");
+        const mapLight = textureLoader.load("../THREE_js/imgs/light2.jpeg");
 
         const material = new THREE.MeshStandardMaterial({
-            map: map
+            map: map,
+            normalMap: mapNormal,
+
+            displacementMap: mapHeight,
+            displacementScale: 0.2,
+            displacementBias: -0.15,
+
+            aoMap: mapAO,
+            aoMapIntensity: 1, //강도 조절. default: 1
+
+            roughnessMap: mapRoughness,
+            toughness: 0.5,  //강도 조절. default: 1
+
+            metalnessMap: mapMetalic,
+            metalness: 0.5,
+
+            alphaMap: mapAlpha,
+            transparent: true,
+            side: THREE.DoubleSide,
+
+            lightMap: mapLight,
+            lightMapIntensity: 2, //강도 조절. default: 1
         });
 
-        const box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material);
+        const box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1, 256, 256, 256), material);
         box.position.set(-1, 0, 0);
+        box.geometry.attributes.uv2 = box.geometry.attributes.uv;
         this._scene.add(box);
+/*
+        const boxHelper = new VertexNormalsHelper(box, 0.1, 0xffff00);
+        this._scene.add(boxHelper); //법선 벡터 시각화 */
 
-        const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.7, 32, 32), material);
+
+        const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.7, 512, 512), material);
         sphere.position.set(1, 0, 0);
+        sphere.geometry.attributes.uv2 = sphere.geometry.attributes.uv;
         this._scene.add(sphere);
+/*
+        const sphereHelper = new VertexNormalsHelper(sphere, 0.1, 0xffff00);
+        this._scene.add(sphereHelper); //법선 벡터 시각화 */
     }
 
     resize() {
