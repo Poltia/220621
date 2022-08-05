158acbad9db47970296b0b5a69e5f904e9691b74
WooJonghwa <wjh423@gmail.com>
2022-08-05 12:50:35 +0900

    .

---
+++ 2022_July/220727/app2.js
@@ -0,0 +1,66 @@
+// 사용할 모듈
+// socketio, express, fs
+
+const express = require("express");
+const socketio = require("socket.io");
+const fs = require("fs")
+
+const app = express();
+
+let seats = [];
+let temp = [
+  [1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],  
+];
+let temp1 = [
+  [1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],  
+];
+let temp2 = [
+  [1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],  
+];
+
+let seatsArr = [temp, temp1, temp2]
+let index = 0;
+const server = app.listen(8080, () => {
+    console.log('8080 server start!')
+})
+
+// socket.io 생성 및 실행
+const io = socketio(server);
+
+app.get("/", (req, res) => {
+  fs.readFile("page2.html", "utf-8", (err, result) => {
+    res.send(result.toString());
+  })
+})
+
+app.get('/seats/:id', (req, res) => { // '/seats' 경로로 seats 배열을 전송
+  index = (req.params.id)
+  seats = seatsArr[index]
+  res.send(seats)
+})
+
+io.sockets.on('connection', (socket) => {
+  console.log("연결되었습니다.");
+  socket.on("reserve", (data) => {
+    let seatsTemp = seatsArr[data.selectcount];
+    seatsTemp[data.y][data.x] = 2;
+    io.sockets.emit("reserve",data)
+  })
+})
+/*과제
+버스나 비행기 모양으로 만들고
+좌석 번호 붙이고
+시간별로 나눠서 바꿀수 있게...
+*/
