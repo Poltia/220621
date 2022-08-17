169d719f14e9250c6139f305b3a6facfd2ba8f8e
WooJonghwa <wjh423@gmail.com>
2022-08-17 19:27:44 +0900

    #2 시청중

--- THREE_js/animation/animation.js
+++ THREE_js/animation/animation.js
@@ -10,10 +10,13 @@ class App {
         const renderer = new THREE.WebGLRenderer({ antialias: true });
         renderer.setPixelRatio(window.devicePixelRatio);
         divContainer.appendChild(renderer.domElement);
+
+        renderer.shadowMap.enabled = true;
+        renderer.shadowMap.type = THREE.VSMShadowMap;
+
         this._renderer = renderer;
 
         const scene = new THREE.Scene();
-        scene.background = new THREE.Color(0xffffff);
         this._scene = scene;
 
         this._setupCamera();
@@ -28,28 +31,62 @@ class App {
     }
 
     _setupControls() {
-        new OrbitControls(this._camera, this._divContainer);
+        this._controls = new OrbitControls(this._camera, this._divContainer);
     }
 
     _setupCamera() {
-        const width = this._divContainer.clientWidth;
-        const height = this._divContainer.clientHeight;
         const camera = new THREE.PerspectiveCamera(
-            40,
-            width / height,
-            100,
-            1000
+            60,
+            window.innerWidth / window.innerHeight,
+            1,
+            5000
         );
-        camera.position.set(0, 0, 300);
+
+        camera.position.set(0, 100, 500);
         this._camera = camera;
     }
 
+    _addPointLight(x, y, z, helperColor) {
+        const color = 0xffffff;
+        const intensity = 1.5;
+
+        const pointLight = new THREE.PointLight(color, intensity, 2000);
+        pointLight.position.set(x, y, z);
+
+        this._scene.add(pointLight);
+
+        const pointLightHelper = new THREE.PointLightHelper(pointLight, 10, helperColor);
+        this._scene.add(pointLightHelper);
+    }
+
     _setupLight() {
-        const color = 0xffffff;  //광원의 색상
-        const intensity = 5;  //광원의 세기
-        const light = new THREE.DirectionalLight(color, intensity);  //색상과 세기로 광원 생성
-        light.position.set(0, 0, 1); //광원의 위치
-        this._scene.add(light);
+        const ambientLight = new THREE.AmbientLight(0xffffff, .5);
+        this._scene.add(ambientLight);
+
+        this._addPointLight(500, 150, 500, 0xff0000);
+        this._addPointLight(-500, 150, 500, 0xffff00);
+        this._addPointLight(-500, 150, -500, 0x00ff00);
+        this._addPointLight(500, 150, -500, 0x0000ff);
+
+        const shadowLight = new THREE.DirectionalLight(0xffffff, 0.2);
+        shadowLight.position.set(200, 500, 200);
+        shadowLight.target.position.set(0, 0, 0);
+        const directionalLightHelper = new THREE.DirectionalLightHelper(shadowLight, 10);
+        this._scene.add(directionalLightHelper);
+
+        this._scene.add(shadowLight);
+        this._scene.add(shadowLight.target);
+
+        shadowLight.castShadow = true;
+        shadowLight.shadow.mapSize.width = 1024;
+        shadowLight.shadow.mapSize.height = 1024;
+        shadowLight.shadow.camera.top = shadowLight.shadow.camera.right = 700;
+        shadowLight.shadow.camera.bottom = shadowLight.shadow.camera.left = -700;
+        shadowLight.shadow.camera.near = 100;
+        shadowLight.shadow.camera.far = 900;
+        shadowLight.shadow.radius = 5;
+        const shadowCameraHelper = new THREE.CameraHelper(shadowLight.shadow.camera);
+        this._scene.add(shadowCameraHelper);
     }
 
     changeAnimation(animationName) {
@@ -94,11 +131,33 @@ class App {
     }
 
     _setupModel() {
+        const planeGeometry = new THREE.PlaneGeometry(1000, 1000);
+        const planeMaterial = new THREE.MeshPhongMaterial({ color: 0x878787 });
+        const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+        plane.rotation.x = -Math.PI/2;
+        this._scene.add(plane);
+        plane.receiveShadow = true;
+
         new GLTFLoader().load("data/Timmy.glb", (gltf) => {
             const Timmy = gltf.scene;
             this._scene.add(Timmy);
 
-            this._setupAnimations(gltf);
+            Timmy.traverse(child => {
+                if(child instanceof THREE.Mesh) {
+                    child.castShadow = true;
+                }
+            });
+
+            const box = (new THREE.Box3).setFromObject(Timmy);
+            Timmy.position.y = (box.max.y - box.min.y) / 2;
+
+            const axisHelper = new THREE.AxesHelper(1000);
+            this._scene.add(axisHelper);
+
+            const boxHelper = new THREE.BoxHelper(Timmy);
+            this._scene.add(boxHelper);
+            this._boxHelper = boxHelper;
+            this._model = Timmy;
         });
     }
 
@@ -121,12 +180,11 @@ class App {
     update(time) {
         time *= 0.001;
 
-        if (this._mixer) {
-            const deltaTime = time - this._previousTime;
-            this._mixer.update(deltaTime);
-        }
+        this._controls.update();
 
-        this._previousTime = time;
+        if (this._boxHelper) {
+            this._boxHelper.update();
+        } // #2 6:35
     }
 }
 
