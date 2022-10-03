440f2b857c1dfa30ef18a174480fdfeddc85979c
WooJonghwa <wjh423@gmail.com>
2022-10-03 10:32:30 +0900

    ...

--- react_proj/src/components/List.js
+++ react_proj/src/components/List.js
@@ -4,13 +4,13 @@ import { useNavigate } from "react-router-dom";
 const List = ({ list, index, setIndexNum }) => {
     console.log("index : ", index);
     const nav = useNavigate();
-    function next() {
+    function post() {
         setIndexNum(index);
         nav(`/post/?=${index}`);
     }
     return (
         <tr>
-            <td className="body_title" onClick={next}>
+            <td className="body_title" onClick={post}>
                 {list.title}
             </td>
             <td className="body_writer">{list.name}</td>
